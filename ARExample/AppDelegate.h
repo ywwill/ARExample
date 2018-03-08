83b851bddef003826aa77df94aa832705fe25a37
yangwei <yangwei@pand-auto.com>
2018-03-08 14:10:43 +0800

    Initial Commit

---
+++ ARExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ARExample
+//
+//  Created by YangWei on 2018/3/8.
+//  Copyright © 2018年 Apple-YangWei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ARExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ARExample
+//
+//  Created by YangWei on 2018/3/8.
+//  Copyright © 2018年 Apple-YangWei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+#import <ARKit/ARKit.h>
+
+@interface ViewController : UIViewController
+
+@end
